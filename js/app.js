2ab93dd44b01d5f92ac5a7618b51e5d967a4257e
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-19 14:03:52 +0100

    ADD: tavolo vip snack

---
+++ js/app.js
@@ -0,0 +1,55 @@
+console.log('gestisci i tavoli')
+
+const tavoloVip = [
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Brad Pitt',
+        posto: 1
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Johnny Depp',
+        posto: 2
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Lady Gaga',
+        posto: 3
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Cristiano Ronaldo',
+        posto: 4
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Georgina Rodriguez',
+        posto: 5
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Chiara Ferragni',
+        posto: 6
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Fedez',
+        posto: 7
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'George Clooney',
+        posto: 8
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Amal Clooney',
+        posto: 9
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: 'Maneskin',
+        posto: 10
+    }
+]
+console.log(tavoloVip)
