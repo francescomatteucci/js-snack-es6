e4b93f6be8959e38273172851f06a1d146ace31c
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-19 15:25:11 +0100

    REFACTOR list user names

--- js/app.js
+++ js/app.js
@@ -53,3 +53,66 @@ const tavoloVip = [
     }
 ]
 console.log(tavoloVip)
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                        //targhette studenti//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const studenti = [
+    {
+        name: 'Marco della Rovere',
+        id: 213,
+        grades: 78
+    },
+    {
+        name: 'Paola Cortellessa',
+        id: 110,
+        grades: 96 
+    },
+    {
+        name: 'Andrea Mantegna',
+        id:250,
+        grades:48
+    },
+    {
+        name: 'Gaia Borromini',
+        id:145,
+        grades:74 
+    },
+    {
+        name: 'Luigi Grimaldello',
+        id:196,
+        grades: 68 
+    },
+    {
+        name: 'Piero della Francesca',
+        id:102,
+        grades:50 
+    },
+    {
+        name: 'Francesca da Polenta',
+        id:120,
+        grades:84 
+    }
+]
+console.log(studenti)
+let nomiStudenti = []
+let currentStudent = 0
+
+const ulDomElement = document.querySelector('.nomi-studenti')
+
+for(let i = 0; i < studenti.length; i++){
+    currentStudent = studenti[i]
+    console.log(currentStudent)
+    nomiStudenti.push(currentStudent.name)
+    
+}
+console.log(nomiStudenti)
+for(let i = 0; i < nomiStudenti.length; i++){
+    const nomeStudente = nomiStudenti[i]
+    ulDomElement.innerHTML += `<li>${nomiStudenti[i].toLowerCase()}</li>`
+}
+
+
