9673d208fc864ca430f5f89e466e54c60218b65f
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-19 16:34:28 +0100

    ADD: method map to snack 1

--- js/app.js
+++ js/app.js
@@ -1,118 +1,108 @@
 console.log('gestisci i tavoli')
 
 const tavoloVip = [
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Brad Pitt',
-        posto: 1
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Johnny Depp',
-        posto: 2
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Lady Gaga',
-        posto: 3
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Cristiano Ronaldo',
-        posto: 4
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Georgina Rodriguez',
-        posto: 5
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Chiara Ferragni',
-        posto: 6
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Fedez',
-        posto: 7
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'George Clooney',
-        posto: 8
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Amal Clooney',
-        posto: 9
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeOspite: 'Maneskin',
-        posto: 10
-    }
-]
-console.log(tavoloVip)
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
 
+const tavoloVipNew = tavoloVip.map(function (nomeOspite, i) { return { nomeTavolo: 'Tavolo VIP', nomeOspite, posto: i++ + 1 }; })
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                        //targhette studenti//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const studenti = [
-    {
-        name: 'Marco della Rovere',
-        id: 213,
-        grades: 78
-    },
-    {
-        name: 'Paola Cortellessa',
-        id: 110,
-        grades: 96 
-    },
-    {
-        name: 'Andrea Mantegna',
-        id:250,
-        grades:48
-    },
-    {
-        name: 'Gaia Borromini',
-        id:145,
-        grades:74 
-    },
-    {
-        name: 'Luigi Grimaldello',
-        id:196,
-        grades: 68 
-    },
-    {
-        name: 'Piero della Francesca',
-        id:102,
-        grades:50 
-    },
-    {
-        name: 'Francesca da Polenta',
-        id:120,
-        grades:84 
-    }
-]
-console.log(studenti)
-let nomiStudenti = []
-let currentStudent = 0
+console.log(tavoloVipNew)
 
-const ulDomElement = document.querySelector('.nomi-studenti')
+const tavoloVip1 = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
 
-for(let i = 0; i < studenti.length; i++){
-    currentStudent = studenti[i]
-    console.log(currentStudent)
-    nomiStudenti.push(currentStudent.name)
-    
-}
-console.log(nomiStudenti)
-for(let i = 0; i < nomiStudenti.length; i++){
-    const nomeStudente = nomiStudenti[i]
-    ulDomElement.innerHTML += `<li>${nomiStudenti[i].toLowerCase()}</li>`
+const tavoloVipNuovo = [];
+for (let i = 0; i < tavoloVip.length; i++) {
+    const vipName = tavoloVip[i];
+    const vipObj = {
+        nomeTavolo: 'Tavolo VIP',
+        nomeOspite: vipName,
+        posto: i + 1
+    };
+    tavoloVipNuovo.push(vipObj);
 }
 
+console.log(tavoloVipNuovo);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//targhette studenti//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// console.log('targhette studenti')
+// const studenti = [
+//     {
+//         name: 'Marco della Rovere',
+//         id: 213,
+//         grades: 78
+//     },
+//     {
+//         name: 'Paola Cortellessa',
+//         id: 110,
+//         grades: 96
+//     },
+//     {
+//         name: 'Andrea Mantegna',
+//         id: 250,
+//         grades: 48
+//     },
+//     {
+//         name: 'Gaia Borromini',
+//         id: 145,
+//         grades: 74
+//     },
+//     {
+//         name: 'Luigi Grimaldello',
+//         id: 196,
+//         grades: 68
+//     },
+//     {
+//         name: 'Piero della Francesca',
+//         id: 102,
+//         grades: 50
+//     },
+//     {
+//         name: 'Francesca da Polenta',
+//         id: 120,
+//         grades: 84
+//     }
+// ]
+// console.log(studenti)
+// let nomiStudenti = []
+// let currentStudent = 0
+
+// const ulDomElement = document.querySelector('.nomi-studenti')
+
+// for (let i = 0; i < studenti.length; i++) {
+//     currentStudent = studenti[i]
+//     console.log(currentStudent)
+//     nomiStudenti.push(currentStudent.name)
+
+// }
+// console.log(nomiStudenti)
+// for (let i = 0; i < nomiStudenti.length; i++) {
+//     const nomeStudente = nomiStudenti[i]
+//     ulDomElement.innerHTML += `<li>${nomiStudenti[i].toLowerCase()}</li>`
+// }
 
