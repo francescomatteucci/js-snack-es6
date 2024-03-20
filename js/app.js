eb0b0bcf29cdf6b6e45f357cb801e85d429ce906
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-20 17:11:23 +0100

    ADD: snack 3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 console.log('gestisci i tavoli')
-
 const tavoloVip = [
     'Brad Pitt',
     'Johnny Depp',
@@ -12,11 +11,8 @@ const tavoloVip = [
     'Amal Clooney',
     'Maneskin'
 ];
-
 const tavoloVipNew = tavoloVip.map(function (nomeOspite, i) { return { nomeTavolo: 'Tavolo VIP', nomeOspite, posto: i++ + 1 }; })
-
 console.log(tavoloVipNew)
-
 // const tavoloVip1 = [
 //     'Brad Pitt',
 //     'Johnny Depp',
@@ -29,7 +25,6 @@ console.log(tavoloVipNew)
 //     'Amal Clooney',
 //     'Maneskin'
 // ];
-
 // const tavoloVipNuovo = [];
 // for (let i = 0; i < tavoloVip.length; i++) {
 //     const vipName = tavoloVip[i];
@@ -40,14 +35,10 @@ console.log(tavoloVipNew)
 //     };
 //     tavoloVipNuovo.push(vipObj);
 // }
-
 // console.log(tavoloVipNuovo);
-
-
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//targhette studenti//
+/////////////////////////////////////////////////TARGHETTE STUDENTI//////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 console.log('targhette studenti')
@@ -93,13 +84,13 @@ const studenti = [
     }
 ]
 const listaNomiStudenti = studenti.map(function (elem) { return {name: elem.name.toLowerCase()} }) 
-console.log(listaNomiStudenti)
+// console.log(listaNomiStudenti)
 ulDomElement.innerHTML = listaNomiStudenti.map(function (elem) { 
     return  `<li>${elem.name}</li>`;
 }).join('')
 
 const over70 = studenti.filter(function (elem) { return elem.grades > 70 })
-console.log(over70)
+// console.log(over70)
 ulDomElement2.innerHTML = over70.map(function(over70){
     return `<li>${over70.name}</li>`
 }).join('')
@@ -107,19 +98,78 @@ const over70GradesAnd120OfId = over70.filter(function (elem) { return elem.id > 
 ulDomElement3.innerHTML = over70GradesAnd120OfId.map(function(over70){
     return `<li>${over70.name}</li>`
 }).join('')
-console.log(over70GradesAnd120OfId)
+// console.log(over70GradesAnd120OfId)
 
 // let nomiStudenti = []
 // let currentStudent = 0
-
 // for (let i = 0; i < studenti.length; i++) {
 //     currentStudent = studenti[i]
 //     console.log(currentStudent)
 //     nomiStudenti.push(currentStudent.name)
-
 // }
 // console.log(nomiStudenti)
 // for (let i = 0; i < nomiStudenti.length; i++) {
 //     const nomeStudente = nomiStudenti[i]
 //     ulDomElement.innerHTML += `<li>${nomiStudenti[i].toLowerCase()}</li>`
 // }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////BICICLETTE//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const bicycles = 
+[
+    {
+        name: 'mountain-bike',
+        weight: 35
+    },
+    {
+        name: 'ARGON 18',
+        weight: 15
+    },
+    {
+        name: 'BERGAMONT',
+        weight: 18
+    },
+    {
+        name: 'BMC',
+        weight: 20
+    }
+]
+console.log(bicycles)
+let lightestBicycle = {
+    weight: 0
+}
+
+bicycles.forEach(function (element){
+    if(element.weight > lightestBicycle.weight){
+        lightestBicycle = element.weight
+       
+    }
+})
+console.log(lightestBicycle)
+
+
+let bikes = []
+
+for(let i = 0; i < 10; i++){
+    bikes[i] = { name: `bici${[i + 1]}` , weight: parseInt(Math.random() * 10) + 30}
+}
+
+let lightestbike = bikes[0]
+
+
+
+for(let i = 0; i < bikes.length; i++){
+    const currentElement = bikes[i]
+    if(currentElement.weight < lightestbike.weight){
+        lightestbike = currentElement
+    }
+}
+console.log(bikes)
+console.log(lightestbike)
+
+const {name, weight} = lightestbike //destre
+console.log(`La bici che pesa di meno è ${name} e ha un peso di ${weight} kg`)
